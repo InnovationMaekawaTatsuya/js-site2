b7b82acec621eb12795531c4a809736ddd204e9c
InnovationMaekawaTatsuya <t_maekawa@innovation.co.jp>
2022-12-05 18:42:28 +0900

    dev load.jsの修正

--- js/load.js
+++ js/load.js
@@ -1,7 +1,7 @@
 // DOM構築が完了したら読み込み
 $(document).ready(function() {
     // 変数 spinner に #loading の要素を格納
-    const spinner = document.getElementById('loading');
+    const spinner = $('#loading')[0];
     // 変数 spinner に .loaded を付与
-    spinner.classList.add('loaded');
+    $(spinner).addClass('loaded');
 });
