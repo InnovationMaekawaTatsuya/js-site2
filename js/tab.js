e0f5ebdcfaad415fce0ccf9718dfb6affbe9ba4c
InnovationMaekawaTatsuya <t_maekawa@innovation.co.jp>
2022-12-06 08:31:19 +0900

    dev js/tab.jsの修正

--- js/tab.js
+++ js/tab.js
@@ -1,26 +1,29 @@
 // DOM構造の読み込みが完了してから読み込み開始
-window.addEventListener('DOMContentLoaded', () => {
+$('document').ready(function() {
     // .tabの要素を取得
-    const tabs = document.getElementsByClassName('tab');
-    // 配列形式の為、個別処理
-    // 「i=1」は使用不可 要素を取り出すときのインデックス番号として使用する為
-    for(let i = 0; i < tabs.length; i++) {
-        // .tabがクリックされたらtabSwitchを実行
-        tabs[i].addEventListener('click', tabSwitch);
-    }
-    // コールバック関数
-    function tabSwitch() {
-        // 現在.is-activeが付与されている要素から.is-activeを除外
-        document.getElementsByClassName('is-active')[0].classList.remove('is-active');
-        // クリックした要素に.is-activeを付与
-        this.classList.add('is-active');
-        // 現在.is-showが付与されている要素から.is-showを除外
-        document.getElementsByClassName('is-show')[0].classList.remove('is-show');
-        // クリックした配列から取り出して格納
-        const arrayTabs = Array.prototype.slice.call(tabs);
-        // indexに要素のインデックス番号を格納
-        const index = arrayTabs.indexOf(this);
-        // クリックした要素のインデックス番号と同じインデックス番号を持つ要素に.is-showを付与
-        document.getElementsByClassName('panel')[index].classList.add('is-show');
+    const tabs = $('.tab');
+    // 配列形式の為、for文で個別処理
+    for (let i = 0; i < tabs.length; i ++) {
+        // 各要素がクリックされたら、
+        $(tabs[i]).on('click', function() {
+            // .is-activeの要素を取得
+            const activeContent = $('.is-active');
+            // .is-activeを除外
+            $(activeContent).removeClass('is-active');
+            // クリックした要素に.is-activeを付与
+            $(this).addClass('is-active');
+            //　.is-showの要素を取得
+            const showContent = $('.is-show');
+            // .is-showを除外
+            $(showContent).removeClass('is-show');
+            // tabsを別の変数に移動
+            const arrayTabs = tabs;
+            // .indexを利用しクリックした要素のインデックス番号をarrayTabsから取得
+            const index = $(arrayTabs).index(this);
+            // .panelの要素を取得
+            const panel = $('.panel');
+            // クリックした要素に.is-showを付与
+            $(panel[index]).addClass('is-show');
+        });
     }
 });
