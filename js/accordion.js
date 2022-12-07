5183c9a40456ffc5d147130eb14e16b9450fccc8
InnovationMaekawaTatsuya <t_maekawa@innovation.co.jp>
2022-12-07 15:17:33 +0900

    dev acordion.js修正

--- js/accordion.js
+++ js/accordion.js
@@ -1,116 +1,65 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     // 関数　open時
-//     const openAccordion = function(e) {
-//         e.style.height = 'auto';
-//         let h = e.offsetHeight;
-//         e.style.height = h + 'px';
-//         e.animate({
-//             height: [ 0, h + 'px' ]
-//         }, {
-//             duration: 500,
-//         });
-//         e.style.height = 'auto';
-//     };
-
-//     // 関数　close時
-//     const closeAccordion = function(e) {
-//         let h = e.offsetHeight;
-//         e.style.height = h + 'px';
-//         e.animate({
-//             height: [ h + 'px', 0 ]
-//         }, {
-//             duration: 500,
-//         });
-//         e.style.height = 0;
-//     };
-
-//     // クリックした要素のインデックス番号を入れる変数
-//     let activeIndex = null;
-
-//     const accordions = document.querySelectorAll('.include-accordion');
-
-//     accordions.forEach( function(accordion) {
-
-//         const accordionBtns = accordion.querySelectorAll('.accordion-btn');
-
-//         accordionBtns.forEach( function(accordionBtn, index) {
-//             accordionBtn.addEventListener('click', function(e) {
-//                 activeIndex = index;
-//                 e.target.parentNode.classList.toggle('active');
-//                 const content = accordionBtn.nextElementSibling;
-//                 if (e.target.parentNode.classList.contains('active')){
-//                     openAccordion(content);
-//                 } else {
-//                     closeAccordion(content);
-//                 }
-//                 accordionBtns.forEach( function(accordionBtn, index) {
-//                     if (activeIndex !== index) {
-//                         accordionBtn.parentNode.classList.remove('active');
-//                         const openedContent = accordionBtn.nextElementSibling;
-//                         closeAccordion(openedContent);
-//                     }
-//                 });
-
-//                 let container = accordion.closest('.scroll-control');
-
-//                 if (accordionBtn.parentNode.classList.contains('active') === false && container !== null ) {
-//                     container.classList.remove('active')
-//                 } else if (container !== null) {
-//                     container.classList.add('active')
-//                 }
-//             });
-//         });
-//     });
-// });
-
 // DOM構造の読み込み完了後、読み込む
-$('document').ready(function() {
+$('document').ready(function(){
     // 関数　open時
-    // const openAccordion = $(function(e) {
-    //     $(e).height('auto');
-    //     let height = $(e).height();
-    //     $(e).height($(height) + 'px');
-    //     $(e).animate({
-    //         height: [ 0, $(height) + 'px' ],
-    //     }, {
-    //         duration: 500,
-    //     });
-    //     $(e).height('auto');
-    // });
+    function openAccordion(obj) {
+        // console.log(obj);
+        const child = obj.next();
+        console.log(child);
+        console.log('open関数発火');
+        child.height('auto');
+        const height = obj.height();
+        child.height(height + 'px');
+        child.animate({
+            height: [ 0, height + 'px' ]
+        },
+            500
+        );
+        child.height('auto');
+    };
 
     // 関数　close時
-    // const closeAccordion = $(function(e) {
-    //     let height = $(e).height();
-    //     $(e).height($(height) + 'px');
-    //     $(e).animate({
-    //         height: [ $(height) + 'px', 0 ],
-    //     }, {
-    //         duration: 500
-    //     });
-    //     $(e).height(0);
-    // });
+    function closeAccordion(e) {
+        // console.log('close関数発火');
+        let height = $(e).height();
+        $(e).height($(height) + 'px');
+        $(e).animate({
+            height: [ $(height) + 'px', 0 ],
+        }, {
+            duration: 500
+        });
+        $(e).height(0);
+    };
 
-    // クリックした要素のインデックス番号を格納する変数
-    let activeIndex = null;
-
-    // アコーディオン要素を格納する変数
     const accordions = $('.include-accordion');
+    // console.log(accordions); // 3つのアコーディオン親要素(ul)
 
-    // 配列の為.eachで個別処理
-    $(accordions).each( function(accordion) {
-
+    // ↑の要素全てに個別処理
+    accordions.each(function() {
         const accordionButtons = $('.accordion-btn');
-        // console.log($(accordionButtons));
+        // console.log(accordionButtons); //.accordion-btnの要素6個取得成功(button)
 
-        for (let [index, accordionButton] in $(accordionButtons)) {
-            // console.log($(accordionButton));
-            $(accordionButton).on('click', function(e) {
-                // console.log('イベント発火');
-                $(activeIndex).text($(index));
-                $(e).parent().toggleClass('active');
-                const content = $(accordionButton).next();
+        accordionButtons.each(function() {
+            $(this).on('click', function() {
+                // console.log($(this));
+                // console.log('イベント発火');　イベント発火の確認
+                $(this).parent().toggleClass('active');
+                let parentClass = $(this).parent();
+                // console.log(parentClass); //クリックした要素の親要素(li)に.activeが付与されているか取得
+                // console.log($(parentClass).hasClass('active'));
+
+                // console.log($('.active'));
+
+                // console.log($('.aaa'));
+
+                // console.log(parentClass.hasClass('active'));
+
+                if ($(parentClass).hasClass('active') === true) {
+                    openAccordion($(this));
+                } else {
+                    closeAccordion($(this));
+                }
             });
-        };
-        // });
+        });
+
     });
 });
